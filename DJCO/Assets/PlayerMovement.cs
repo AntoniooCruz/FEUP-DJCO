eafcc09b12109e4e0078fb3202d0deac2c8bc24e
Antonio Cruz <acruz9823@gmail.com>
2020-02-29 18:38:22 +0000

    MovementUpdate with Boundaries

---
+++ DJCO/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+ 
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        var pos = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+        pos.x = Mathf.Clamp(pos.x, -9f, 4f);
+        pos.y = Mathf.Clamp(pos.y, -4f, 4f);
+
+        rb.MovePosition(pos);
+    }
+    
+}
