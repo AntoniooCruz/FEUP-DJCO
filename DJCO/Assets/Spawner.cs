04cde18455e7ee017d2e2c6ce16aed8775b8d012
Antonio Cruz <acruz9823@gmail.com>
2020-03-01 21:15:39 +0000

    Added Spawner of PowerUps

---
+++ DJCO/Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject rapidFire;
+    public Transform spawner;
+    private float powerUpTimer = 0.0f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        InvokeRepeating("createRapidFire",10.0f,10.0f);
+    }
+
+    void createRapidFire()
+    {
+        GameObject go = Instantiate(rapidFire, new Vector3(spawner.transform.position.x, Random.Range(-4f, 4f)), spawner.rotation);
+    }
+}
