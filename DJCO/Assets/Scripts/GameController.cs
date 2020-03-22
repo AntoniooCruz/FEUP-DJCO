94b2465f033211670f8428b0668d0c38006e6882
Pedro <lopes.pedroa@gmail.com>
2020-03-22 19:43:57 +0000

    Fixed bug on GC

--- DJCO/Assets/Scripts/GameController.cs
+++ DJCO/Assets/Scripts/GameController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
 
     public static GameController instance;
+
+    private void Awake() {
+        instance = this;
+    }
     
     public void KillPlayer(Player player)
     {
@@ -15,5 +20,9 @@ public class GameController : MonoBehaviour
     public void KillEnemy(EnemyShip enemy)
     {
         Destroy(enemy.gameObject);
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene("Game");
     }
 }
